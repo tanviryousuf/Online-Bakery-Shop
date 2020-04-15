ddaecf47be3ae824ca0ed8f7043c49d5c3091637
tanviryousuf <30272618+tanviryousuf@users.noreply.github.com>
2020-04-16 00:27:04 +0600

    refresh

---
+++ Interface/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TanvirBakery.Interface
+{
+    public interface IRepository<TEntity> where TEntity:class
+    {
+
+        TEntity GetById(int id);
+        IEnumerable<TEntity> GetAll();
+
+        void Insert(TEntity entity);
+        void Delete(TEntity entity);
+        int Submit();
+        void Update(TEntity entity);
+
+
+
+
+
+
+
+    }
+}
